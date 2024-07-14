e6c4a02566d05e829b45ad9cd43ee3f731facbc9
serfg <sergei.pozdnyakov@gmail.com>
2024-07-14 17:58:23 +0200

    changing cpp extension location engine

--- src/__init__.py
+++ src/__init__.py
@@ -6,9 +6,9 @@ import importlib.resources as pkg_resources
 def load_neighbors_convert():
     try:
         # Locate the shared object file in the package
-        lib_path = pkg_resources.path(__name__, 'neighbors_convert.so')
-        # Load the shared object file
-        torch.ops.load_library(str(lib_path))
+        with pkg_resources.files(__name__).joinpath('neighbors_convert.so') as lib_path:
+            # Load the shared object file
+            torch.ops.load_library(str(lib_path))
     except Exception as e:
         print(f"Failed to load neighbors_convert.so: {e}")
 
